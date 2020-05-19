01e75bec84316e4dd42061b4f49d39ca45229e53
marchenko2k16 <lexandrmarch@gmail.com>
2020-05-20 01:37:53 +0300

    reset repo

--- Source/AStarProject/AStarProject.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-#include "AStarProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AStarProject, "AStarProject" );
-
-DEFINE_LOG_CATEGORY(LogAStarProject)
- 
